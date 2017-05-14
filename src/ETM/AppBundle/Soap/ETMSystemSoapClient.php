aaf91d6fe53f262d5863a3887a207a27c561ebe3
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2017-05-15 00:40:15 +0200

    [task] Сделал клиент и фабрику

---
+++ src/ETM/AppBundle/Soap/ETMSystemSoapClient.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace ETM\AppBundle\Soap;
+
+use RobRichards\WsePhp\WSSESoap;
+use RobRichards\XMLSecLibs\XMLSecurityKey;
+
+class MySoap extends \SoapClient
+{
+    private $pathToCert;
+    private $pathToPrivateKey;
+
+    protected function getPathToCert()
+    {
+        return $this->pathToCert;
+    }
+
+    public function setPathToCert($pathToCert)
+    {
+        $this->pathToCert = $pathToCert;
+        return $this;
+    }
+
+    protected function getPathToPrivateKey()
+    {
+        return $this->pathToPrivateKey;
+    }
+
+    public function setPathToPrivateKey($pathToPrivateKey)
+    {
+        $this->pathToPrivateKey = $pathToPrivateKey;
+        return $this;
+    }
+
+    protected function getCertContent()
+    {
+        return file_get_contents($this->getPathToCert());
+    }
+
+    function __doRequest($request, $location, $saction, $version, $q = 0) {
+
+        $doc = new \DOMDocument('1.0');
+        $doc->loadXML($request);
+        $objWSSE = new WSSESoap($doc);
+
+        $objWSSE->addTimestamp();
+
+        $objKey = new XMLSecurityKey(XMLSecurityKey::RSA_SHA1, ['type'=>'private']);
+
+        $objKey->loadKey($this->getPathToPrivateKey(), true);
+
+        $objWSSE->signSoapDoc($objKey);
+
+        $token = $objWSSE->addBinaryToken($this->getCertContent());
+        $objWSSE->attachTokentoSig($token);
+
+        return parent::__doRequest($objWSSE->saveXML(), $location, $saction, $version);
+    }
+}
---
+++ src/ETM/AppBundle/Soap/ETMSystemSoapClientFactory.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User: dpushnitsa
+ * Date: 5/15/17
+ * Time: 12:32 AM
+ */
+
+namespace ETM\AppBundle\Soap;
+
+class ETMSystemSoapClientFactory
+{
+
+}
