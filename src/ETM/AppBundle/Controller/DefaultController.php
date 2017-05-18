15539c52b4a83064bb8afe7076dcaed153a84348
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2017-05-18 23:37:54 +0200

    [task] Настроил маршруты

--- src/ETM/AppBundle/Controller/DefaultController.php
+++ src/ETM/AppBundle/Controller/DefaultController.php
@@ -3,11 +3,23 @@
 namespace ETM\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+
         return $this->render('AppBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("ping")
+     */
+    public function pingAction()
+    {
+        $communicator = $this->get('etm_system_communicator');
+        return new JsonResponse($communicator->ping());
     }
 }
