7ce91fb272a69c3b8f9c0d23fb7830b6f766953c
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2017-05-20 16:09:48 +0200

    [task] Добавил IATAFetcher

---
+++ src/ETM/AppBundle/IATA/IATAFetcher.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ETM\AppBundle\IATA;
+
+use Symfony\Component\Filesystem\Filesystem;
+
+class IATAFetcher
+{
+    protected $fileSystem;
+    protected $IATAJson;
+
+    public function __construct(Filesystem $filesystem, $pathToIATAJson)
+    {
+        $this->fileSystem = $filesystem;
+        $this->IATAJson = $pathToIATAJson;
+    }
+
+    public function fetch()
+    {
+        if (! $this->isFileExists()) {
+            throw new \RuntimeException(sprintf("Файл \"%s\" не существует", $this->IATAJson));
+        }
+
+        return file_get_contents($this->IATAJson);
+    }
+
+    protected function isFileExists()
+    {
+        return $this->fileSystem->exists($this->IATAJson);
+    }
+}
